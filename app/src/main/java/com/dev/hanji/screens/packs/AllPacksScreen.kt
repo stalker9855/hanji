ee7c59d233b21dc0cccb55c89863a20375cb8d3f
stalker9855 <andrey.l.evseev@gmail.com>
2025-03-02 09:25:11 +0200

    MyPacksScreen and PacksScreen

--- app/src/main/java/com/dev/hanji/screens/packs/AllPacksScreen.kt
+++ app/src/main/java/com/dev/hanji/screens/packs/AllPacksScreen.kt
@@ -1,5 +1,6 @@
 package com.dev.hanji.screens.packs
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,11 +10,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,16 +39,23 @@ import androidx.compose.ui.unit.sp
 fun AllPacksScreen(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
+            .padding(top = 8.dp)
+            .verticalScroll(rememberScrollState()),
     ) {
+        PackItem()
+        PackItem()
+        PackItem()
+        PackItem()
         PackItem()
     }
 }
 
 @Composable
 fun PackItem(modifier: Modifier = Modifier) {
+    val checked = remember { mutableStateOf(false) } // temporary value
     Row(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(horizontal = 16.dp, vertical = 4.dp)
             .clip(RoundedCornerShape(16.dp))
             .fillMaxWidth()
             .background(MaterialTheme.colorScheme.surfaceContainer)
@@ -57,15 +76,34 @@ fun PackItem(modifier: Modifier = Modifier) {
         Row(
             modifier = Modifier
                 .padding(16.dp)
-                .clip(RoundedCornerShape(16.dp))
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.primaryContainer),
+                //.clip(RoundedCornerShape(16.dp))
+                .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceAround
         ) {
-            Text("Elements")
-            Row {
-                Text("Kanji: 12")
-                Text("Difficulty: Hard")
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Elements",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                HorizontalDivider(modifier = Modifier.padding(8.dp))
+                Row(
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    Column {
+                        Text("Kanji Count: 12")
+                        Text("Difficulty: Hard")
+                    }
+                    IconToggleButton(
+                        modifier = Modifier.background(MaterialTheme.colorScheme.surfaceContainer),
+                        checked = checked.value,
+                        onCheckedChange = { checked.value = it }
+                        ) {
+                        Icon(imageVector = Icons.Filled.Favorite, contentDescription = "")
+
+                    }
+                }
             }
         }
     }
